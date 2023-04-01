f7f1a7e295b2bede838dbbbb80ff3f186ba242a1
Pat Tran <pat.tran.266@gmail.com>
2023-04-01 19:27:58 -0400

    add the Pressure calculation

---
+++ PHL/Calculation/Pressure.swift
@@ -0,0 +1,25 @@
+//
+//  Pressure.swift
+//  PHL
+//
+//  Created by Tran Pat on 4/1/23.
+//  Following the VibroScale paper
+//
+
+import Foundation
+
+class Pressure {
+    static func calculateIntensity(y_accelerometer: [Double]) -> Double {
+        // subtracting all the values from the input array by the average
+        var average = Double(y_accelerometer.reduce(0, +)) / Double(y_accelerometer.count)
+        var new_y_accelerometer : [Double] = []
+        for i in 0..<y_accelerometer.count {
+            new_y_accelerometer.append(y_accelerometer[i] - average)
+        }
+        // take the absolute value for all values
+        new_y_accelerometer = new_y_accelerometer.map(abs)
+        // the mean of this abs array is the intensity
+        var intensity = Double(new_y_accelerometer.reduce(0, +)) / Double(new_y_accelerometer.count)
+        return intensity;
+    }
+}
